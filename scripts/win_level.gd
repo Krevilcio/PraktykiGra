f7b769a6f678684a02d2d177393d52c02214b82c
Krevilcio <105020965+Krevilcio@users.noreply.github.com>
2024-09-27 18:09:16 +0200

    Update win_level.gd

--- scripts/win_level.gd
+++ scripts/win_level.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-@onready var coin_label: Label = $GameManager/AmountOfCoins  # Upewnij się, że ścieżka do Labela jest poprawna
-
+@onready var coin_label: Label = $GameManager/AmountOfCoins
 func _ready():
 	
 	coin_label.text = str(GameManager.score) + " coins!"
